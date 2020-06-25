381af7fa96531a5ae3bdf2f0dc1192ff335feee4
Samuel Gemberling <sgembeling@gmail.com>
2020-06-25 13:35:51 -0500

    prevent multiple photo upload

--- main_app/views.py
+++ main_app/views.py
@@ -14,16 +14,18 @@ BUCKET = 'gamefaves'
 
 def add_photo(request, game_id):
   photo_file = request.FILES.get('photo-file', None)
-  if photo_file:
-    s3 = boto3.client('s3')
-    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-    try:
-      s3.upload_fileobj(photo_file, BUCKET, key)
-      url = f"{S3_BASE_URL}{BUCKET}/{key}"
-      photo = Photo(url=url, game_id=game_id)
-      photo.save()
-    except:
-      print('An error occurred uploading file to S3')
+  game_photo = Photo.objects.get(game=game_id)
+  if !game_photo:
+    if photo_file:
+      s3 = boto3.client('s3')
+      key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+      try:
+        s3.upload_fileobj(photo_file, BUCKET, key)
+        url = f"{S3_BASE_URL}{BUCKET}/{key}"
+        photo = Photo(url=url, game_id=game_id)
+        photo.save()
+      except:
+        print('An error occurred uploading file to S3')
   return redirect('detail', game_id=game_id)
 
 def home(request):
